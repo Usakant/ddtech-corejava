dea9a37d19984dfc5f9662a2510723568300ee51
Usakant <umajhee.2012@gmail.com>
2024-11-28 07:42:22 +0530

    interview program

--- src/javaeight/StreamAPI.java
+++ src/javaeight/StreamAPI.java
@@ -1,5 +1,6 @@
 package javaeight;
 
+
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
@@ -10,13 +11,15 @@ import static java.util.stream.Collectors.counting;
 import static java.util.stream.Collectors.groupingBy;
 
 public class StreamAPI {
-
+    List<Integer> list = Arrays.asList(2, 4, 5,3, 6);
+    int sumOfEvenNumber = list.stream().filter(a -> a%2 !=0).mapToInt(n -> n*n).sum();
+    int sumOfOddNumber = list.stream().filter(a -> a%2 ==0).mapToInt(n -> n*n).sum();
     List<Transaction> transactions = Arrays.asList(new Transaction(100),new Transaction(200) ) ;
     // use of filter , map and reduce
     int sumExpensive =
             transactions.stream()
                     .filter(t -> t.getValue() > 100)
-                    .map(Transaction::getValue)
+                    . map(Transaction::getValue)
                     .reduce(50, Integer::sum);
 
 
@@ -27,7 +30,7 @@ public class StreamAPI {
     Map<String, Long> letterToCount =
             words.map(w -> w.split(""))
                     .flatMap(Arrays::stream)
-                    .distinct()
+                    //.distinct()
                     .collect(groupingBy(identity(), counting()));
 
     public static void main(String[] args) {
@@ -36,5 +39,11 @@ public class StreamAPI {
 
         System.out.println("---------------");
         System.out.println("flatmap "+obj.letterToCount);
+
+        System.out.println("---------------");
+        System.out.println("sum of Even number "+obj.sumOfEvenNumber);
+
+        System.out.println("---------------");
+        System.out.println("sum of odd number "+obj.sumOfOddNumber);
     }
 }
